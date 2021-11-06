b7b04c0df3423f88fb10c2d2b4c2e52dae6daf52
emrecanAy <codemrecan@gmail.com>
2021-11-06 18:47:21 +0300

    Heroku konfigürasyonu yapıldı!

--- src/main/java/com/emrecan/springbootbookseller/SpringBootBookSellerApplication.java
+++ src/main/java/com/emrecan/springbootbookseller/SpringBootBookSellerApplication.java
@@ -3,10 +3,12 @@ package com.emrecan.springbootbookseller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class SpringBootBookSellerApplication {
 
 	public static void main(String[] args) {
